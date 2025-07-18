464fc6d95196cbc5b861c39ecd259cd2268f578c
new-username <ashish061104@gmail.com>
2025-07-18 13:39:59 +0530

    person card ui changes

--- client/src/components/PersonCard.jsx
+++ client/src/components/PersonCard.jsx
@@ -1,41 +1,36 @@
 import Aurora from "../animations/Aurora";
+import GlareHover from "../animations/GlareHover";
 
 
 const PersonCard = ({ person }) => {
   if (!person) return null;
 
   return (
-    <div className="relative p-4 rounded-xl overflow-hidden border text-white">
-      {/* <div className="absolute inset-0 -z-10">
+    <div className="relative p-2 rounded-xl overflow-hidden text-white flex justify-center" >
+      <div className="absolute inset-0 -z-10">
         <Aurora
-          colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
-          blend={0.5}
-          amplitude={1.0}
-          speed={0.5}
+          colorStops={["#2978ff", "#2978ff", "#2978ff"]}
+          blend={1}
+          amplitude={2.0}
+          speed={1}
         />
-      </div> */}
-      <h2 className="text-xl font-semibold mb-2 bg-transparent">Selected Person</h2>
-      {person.image && (
-        <img
-          src={person.image}
-          alt={person.name}
-          className="w-10 h-10 mb-2 rounded"
-        />
-      )}
-      <p><strong>Name:</strong> {person.name}</p>
-      <p><strong>Description:</strong> {person.description}</p>
-      <p>
-        <strong>Source:</strong>{" "}
-        <a
-          className="text-blue-600"
-          href={person.url}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {person.url}
-        </a>
-      </p>
+      </div>
+      <GlareHover>
+        <h2 className="text-2xl font-semibold mb-2 bg-transparent">Selected Person</h2>
 
+        <p><strong>Name:</strong> {person.name}</p>
+        <p><strong>Description:</strong> {person.description}</p>
+        <p>
+          <strong>Source:</strong>{" "}
+          <a
+            href={person.url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {person.url}
+          </a>
+        </p>
+      </GlareHover>
     </div>
   );
 };
